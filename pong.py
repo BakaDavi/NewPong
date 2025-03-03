35efe80906c9d9985b5d276592fd98cb90e90a43
Davide Cernuto <davide@cernuto.net>
2025-03-03 13:02:54 +0100

    Clean pong.py

--- pong.py
+++ pong.py
@@ -7,42 +7,35 @@ from enums import MyEnum
 import pong_client
 
 class GameApp:
-    def __init__(self, game_width, game_heigth): 
-        
+    def __init__(self, game_width, game_height):
+        # Initialize game managers
         self.tick_manager = TickManager()
         self.render_manager = RenderManager()
-        self.client = pong_client.PongClient(self.tick_manager,self.render_manager)
+        
+        # Create client for multiplayer functionality
+        self.client = pong_client.PongClient(self.tick_manager, self.render_manager)
 
-
-        #Create client
-
-        #self.ball = ball.BallBase(80, 60, 4, 1, 1, self.points_manager)
-
-        #self.player1.register_to_managers([self.tick_manager, self.render_manager, self.physics_manager])
-        #self.player2.register_to_managers([self.tick_manager, self.render_manager, self.physics_manager])
-        #self.ball.register_to_managers([self.tick_manager, self.render_manager, self.physics_manager])
-
-        pyxel.init(game_width, game_heigth)
+        # Initialize pyxel game engine with specified dimensions
+        pyxel.init(game_width, game_height)
         pyxel.run(self.tick, self.render)
 
     def tick(self):
+        """Update game state each frame"""
         self.tick_manager.manage()
         self.client.run_client()
-        #run client
-        #self.physics_manager.manage()
-        # if pyxel.btnp(pyxel.KEY_F):
-        #     self.save()
-        # if pyxel.btnp(pyxel.KEY_R):
-        #     self.reset()
+        
+        # Check for quit key press
         if pyxel.btnp(pyxel.KEY_Q):           
            pyxel.quit()
         
     def render(self):
-        pyxel.cls(0)
+        """Draw game elements to the screen"""
+        pyxel.cls(0)  # Clear screen with black background
         self.render_manager.manage()
-        #pyxel.rect(10,10,20,20,11)
 
     def save(self):
+        """Save player stats to persistent storage"""
+        # Create entries for players if they don't exist
         if self.player1.name not in self.game_save.players_data:
             self.game_save.players_data[self.player1.name] = 0
         if self.player2.name not in self.game_save.players_data:
@@ -50,16 +43,17 @@ class GameApp:
         self.game_save.save_data()
 
     def reset(self):
+        """Reset game state to initial conditions"""
         self.player1.reset()
         self.player2.reset()
         self.ball.reset()
         self.game_save.reset()
-        
-
 
     def load_data(self):
+        """Load previously saved game data"""
         self.game_save.load_data()
         if not self.game_save.is_new_game:
+            # Extract player names and scores from saved data
             names = list(self.game_save.players_data.keys())
             points = list(self.game_save.players_data.values())
             self.player1.name = names[0]
@@ -69,9 +63,6 @@ class GameApp:
         else:
             self.game_save.players_data = {}
 
-
-    #pyxel.run(update,draw)
 if __name__ == "__main__":
-    #create GameSave object
-        
-    GameApp(160,120)
+    # Create game instance with 160x120 resolution
+    GameApp(160, 120)
